e75e19178c4a6acc59b059b8523d0efe947e1d10
Lyzen Vira <133158996+LyzenVira@users.noreply.github.com>
2023-11-21 11:46:09 +0200

    feature: add ocelot

---
+++ SL_OcelotApiGetAway/Program.cs
@@ -0,0 +1,37 @@
+
+using Ocelot.DependencyInjection;
+
+namespace SL_OcelotApiGetAway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+            builder.Services.AddOcelot(builder.Configuration);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
